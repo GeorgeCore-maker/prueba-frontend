19054ab60f096f68aaa0668c09e28e7c2e427e61
Jorge Diaz <jorgediazal.ing@gmail.com>
2025-02-25 19:28:29 -0500

    creacion de los archivos environment

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,  // Indica que es un entorno de producción
+  apiUrl: 'https://api.prueba-frontend.com',  // Ejemplo de URL de API para producción
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,  // Indica que es un entorno de desarrollo
+  apiUrl: 'http://localhost:4200',  // Ejemplo de URL de API para desarrollo
+};
